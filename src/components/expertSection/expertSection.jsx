7b683814e8bbec592f487eb8559e947b77765186
sachinragul <sachin.r@navadhiti.com>
2023-05-17 17:59:07 +0530

    worked on expert section

---
+++ src/components/expertSection/expertSection.jsx
@@ -0,0 +1,109 @@
+import { Stack, styled, Box, useMediaQuery } from "@mui/material";
+import { PrimaryText, PrimarySubText } from "../common/typographies/typographies";
+import { TextCenterBox } from "../common/containers/containers";
+import expertImage from "../../assets/images/expertsection.jpg";
+
+const ExpertSectionMainBox = styled(TextCenterBox)(({ theme }) => ({
+  height: 390,
+  width: "auto",
+  backgroundColor: theme.palette.expertColor.main,
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    height: 262,
+    width: "auto",
+  },
+}));
+
+const Title = styled(PrimaryText)(({ theme }) => ({
+  fontSize: 40,
+  fontWeight: 500,
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 20,
+  },
+}));
+const SubTitle = styled(PrimarySubText)(({ theme }) => ({
+  paddingBottom: 20,
+  textAlign: "center",
+
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 12,
+    padding: "0px 31.5px 20px 31.5px",
+  },
+}));
+const ContentBox = styled(TextCenterBox)(({ theme }) => ({
+  backgroundColor: theme.palette.textSecondary.main,
+  width: 607,
+  [theme.breakpoints.down("sm")]: {
+    height: 100,
+    width: 341,
+  },
+}));
+
+const ImageBox = styled(Box)(({ theme }) => ({
+  // background: "red",
+  backgroundImage: `url(${expertImage})`,
+  height: 213,
+  width: 213,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  [theme.breakpoints.down("sm")]: {
+    height: 100,
+    width: 100,
+  },
+}));
+const ContentBoxTitle = styled(PrimaryText)(({ theme }) => ({
+  fontWeight: 500,
+  fontSize: 25,
+  paddingBottom: 10,
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 16,
+  },
+}));
+
+const ContentSubBox = styled(TextCenterBox)(({ theme }) => ({
+  height: 213,
+  width: 394,
+  backgroundColor: theme.palette.textSecondary.main,
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    height: 100,
+    width: 241,
+  },
+}));
+
+const SubContainerBox = styled(TextCenterBox)(({ theme }) => ({
+  height: 49,
+  width: 274,
+  border: `1px solid ${theme.palette.primary.main}`,
+  cursor: "pointer",
+  [theme.breakpoints.down("sm")]: {
+    height: 32,
+    width: 149,
+  },
+}));
+const SubContainerBoxText = styled(PrimarySubText)(({ theme }) => ({
+  textAlign: "center",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 10,
+  },
+}));
+export const ExpertSection = () => {
+  return (
+    <ExpertSectionMainBox>
+      <Title color={"secondary"}> Is this the best solution for me?</Title>
+      <SubTitle variant="secondaryText" color={"textPrimary"}>
+        Take our expert doctor's assessment to find out if this is the right solution for
+      </SubTitle>
+      <ContentBox>
+        <ImageBox />
+        <ContentSubBox>
+          <ContentBoxTitle color={"secondary"}>Talk to our experts</ContentBoxTitle>
+          <SubContainerBox>
+            <SubContainerBoxText color={"primary"}>Take Quick Assessment</SubContainerBoxText>
+          </SubContainerBox>
+        </ContentSubBox>
+      </ContentBox>
+    </ExpertSectionMainBox>
+  );
+};
---
+++ src/components/expertSection/index.js
@@ -0,0 +1,1 @@
+export { ExpertSection } from "./expertSection";
