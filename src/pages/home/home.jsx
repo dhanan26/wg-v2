11193b7824bc18cf21223f18f54b1c7de995dd19
elvin-nd <elvin@navadhiti.com>
2023-05-18 17:26:04 +0530

    add success stories

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,7 @@ import { ExploreExpert } from "../../components/expoloreExpert";
 import { FaqSection } from "../../components/faqSection";
 import { Footer } from "../../components/footer";
 import { PopularPackage } from "../../components/popularPackage";
+import { SuccessStories } from "../../components/successStories/successStories";
 
 export const Home = () => {
   return (
@@ -10,6 +11,7 @@ export const Home = () => {
       <PopularPackage />
       <ExploreExpert />
       <Content />
+      <SuccessStories />
       <FaqSection />
       <Footer />
     </>
