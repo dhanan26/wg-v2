ae2bb9b7ca72580f34e9ae98a91315f57b50f901
@yunus-ze <yunus@zenequity.com>
2023-05-19 16:20:19 +0530

    Why Us overlapping issue fixed.

--- src/components/whyUs/whyUs.jsx
+++ src/components/whyUs/whyUs.jsx
@@ -1,34 +1,36 @@
 import styled from "@emotion/styled";
-import { Box, Grid, useMediaQuery } from "@mui/material";
-import { TextCenterBox, GridBox } from "../common/containers/containers";
+import { Box, useMediaQuery } from "@mui/material";
+import { TextCenterBox } from "../common/containers/containers";
 import {
   PrimarySubText,
   PrimaryText,
 } from "../common/typographies/typographies";
-import { whyUsImageData } from "../../data/whyUs";
-import ImageListItem from "@mui/material/ImageListItem";
 import image1 from "../../assets/images/Why Us/Why_Us-1.png";
 import image2 from "../../assets/images/Why Us/Why_Us-2.png";
 import image3 from "../../assets/images/Why Us/Why_Us-3.png";
 
 const WhyUsBox = styled(TextCenterBox)(({ theme }) => ({
-  height:209,
-  backgroundColor:"red"
+  padding: "00px 0px 0px 0px",
+  height: 417,
+
+  [theme.breakpoints.down(`smd`)]: {
+    height: 434,
+  },
 }));
 
 const WhyUsTitleBox = styled(TextCenterBox)(({ theme }) => ({
   flexDirection: "column",
   textAlign: "center",
-  gap: 10,
+  rowGap: 10,
 }));
 
 const WhyUsTitle = styled(PrimaryText)(({ theme }) => ({
-  padding: "40px 0px 0px 0px",
+  padding: "0px 0px 0px 0px",
   fontSize: 40,
   color: theme.palette.primary.main,
   [theme.breakpoints.down("smd")]: {
     fontSize: 20,
-    padding: "30px 0px 0px 0px",
+    padding: "0px 0px 0px 0px",
   },
 }));
 
@@ -57,13 +59,15 @@ const WhyUsSubText = styled(PrimarySubText)(({ theme }) => ({
 const CustomImageBox = styled(Box)(({ theme }) => ({
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
-  gap: 1,
+  gap: "40px",
+  paddingTop: "10px",
   gridTemplateRows: "auto",
   gridTemplateAreas: `"main footer sidebar "`,
   justifyContent: "center",
   [theme.breakpoints.down("smd")]: {
     gridTemplateColumns: "repeat(4, 1fr)",
-    gap: 1,
+    columnGap: 15,
+    rowGap: 20,
     gridTemplateRows: "auto",
     gridTemplateAreas: `
         "main main sidebar sidebar"
@@ -74,70 +78,54 @@ const CustomImageBox = styled(Box)(({ theme }) => ({
 export const WhyUS = () => {
   const isSmallScreen = useMediaQuery("(max-width:800px)");
   return (
-    <>
-      <WhyUsBox>
-        <WhyUsTitleBox>
-          <WhyUsTitle variant="primaryTitle" color="secondary">
-            Why Us?
-          </WhyUsTitle>
-          <WhyUsSubTitle variant="secondaryTitle" color="textPrimary">
-            We offer more than just clinically proven beauty products, and
-            provide holistic care for hair and nutrition led by trichologists
-            and dieticians
-          </WhyUsSubTitle>
+    <WhyUsBox>
+      <WhyUsTitleBox>
+        <WhyUsTitle variant="primaryTitle" color="secondary">
+          Why Us?
+        </WhyUsTitle>
+        <WhyUsSubTitle variant="secondaryTitle" color="textPrimary">
+          We offer more than just clinically proven beauty products, and provide
+          holistic care for hair and nutrition led by trichologists and
+          dieticians
+        </WhyUsSubTitle>
 
+        <CustomImageBox>
+          <Box sx={{ gridArea: "main" }}>
+            {isSmallScreen ? (
+              <img src={image1} alt="image1" loading="lazy" width={100} />
+            ) : (
+              <img src={image1} alt="image1" loading="lazy" />
+            )}
+            <WhyUsSubText color="textPrimary">
+              We understand your needs
+            </WhyUsSubText>
+          </Box>
+          <Box sx={{ gridArea: "sidebar" }}>
+            {isSmallScreen ? (
+              <img src={image2} alt="image1" loading="lazy" width={100} />
+            ) : (
+              <img src={image2} alt="image1" loading="lazy" />
+            )}
+            <WhyUsSubText color="textPrimary">
+              We understand your needs
+            </WhyUsSubText>
+          </Box>
           <Box
             sx={{
-              width: "100%",
-              height: "140px",
-              color: "#fff",
-              "& > .MuiBox-root > .MuiBox-root": {
-                p: 1,
-                borderRadius: 2,
-                fontSize: "0.875rem",
-                fontWeight: "700",
-              },
+              gridArea: "footer",
             }}
           >
-            <CustomImageBox>
-              <Box sx={{ gridArea: "main" }}>
-                {isSmallScreen ? (
-                  <img src={image1} alt="image1" loading="lazy" width={100} />
-                ) : (
-                  <img src={image1} alt="image1" loading="lazy" />
-                )}{" "}
-                <WhyUsSubText color="textPrimary">
-                  We understand your needs
-                </WhyUsSubText>
-              </Box>
-              <Box sx={{ gridArea: "sidebar" }}>
-                {isSmallScreen ? (
-                  <img src={image2} alt="image1" loading="lazy" width={100} />
-                ) : (
-                  <img src={image2} alt="image1" loading="lazy" />
-                )}
-                <WhyUsSubText color="textPrimary">
-                  We understand your needs
-                </WhyUsSubText>
-              </Box>
-              <Box
-                sx={{
-                  gridArea: "footer",
-                }}
-              >
-                {isSmallScreen ? (
-                  <img src={image3} alt="image1" loading="lazy" width={100} />
-                ) : (
-                  <img src={image3} alt="image1" loading="lazy" />
-                )}
-                <WhyUsSubText color="textPrimary">
-                  We understand your needs
-                </WhyUsSubText>
-              </Box>
-            </CustomImageBox>
+            {isSmallScreen ? (
+              <img src={image3} alt="image1" loading="lazy" width={100} />
+            ) : (
+              <img src={image3} alt="image1" loading="lazy" />
+            )}
+            <WhyUsSubText color="textPrimary">
+              We understand your needs
+            </WhyUsSubText>
           </Box>
-        </WhyUsTitleBox>
-      </WhyUsBox>
-    </>
+        </CustomImageBox>
+      </WhyUsTitleBox>
+    </WhyUsBox>
   );
 };
