79a8242cb029bdb5e21fb6f8e6c02c2959262cc2
elvin-nd <elvin@navadhiti.com>
2023-05-24 17:35:04 +0530

    add the package card

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,94 +1,119 @@
-
-import { Stack, styled, Box, useMediaQuery, FormControlLabel, Grid } from "@mui/material";
-import { borderLeft } from "@mui/system";
+import { styled, Box, useMediaQuery, Grid } from "@mui/material";
 import { StyledSwitch } from "../common/switch";
-import { PrimarySubText } from "../common/typographies/typographies"
+import { PrimarySubText } from "../common/typographies/typographies";
 import { Filter } from "../filters";
 import { RecommendedSpecialties } from "../recommendedSpecialties";
 import { EnquiryForm } from "../enquiryForm";
+import { PackageCard } from "../widgets/packageCard";
 
-
-const MainContentContainer = styled((props) => (
-    <Grid container {...props} />
-))(({ theme }) => ({
-    [theme.breakpoints.down('sm')]: {
-        paddingLeft: 16,
-        paddingRight: 16
-    },
-    height: "100vh",
-    paddingLeft: 80,
-    paddingRight: 36
+const MainContentContainer = styled((props) => <Grid container {...props} />)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  height: "100vh",
+  paddingLeft: 80,
+  paddingRight: 36,
 }));
 
-
-const FilterContainer = styled((props) => (
-    <Grid item {...props} md={2.5} sm={0} />
-))(({ theme }) => ({
-    paddingRight: 24,
-    [theme.breakpoints.down('md')]: {
-        paddingLeft: 10
-    },
+const FilterContainer = styled((props) => <Grid item {...props} md={2.5} sm={0} />)(({ theme }) => ({
+  paddingRight: 24,
+  [theme.breakpoints.down("md")]: {
+    paddingLeft: 10,
+  },
 }));
 
-const PartnerContainer = styled((props) => (
-    <Grid item {...props} md={9.5} sm={12} xs={12} />
-))(({ theme }) => ({
-    backgroundColor: "red"
+const PartnerContainer = styled((props) => <Grid item {...props} md={9.5} sm={12} xs={12} />)(() => ({
+  // backgroundColor: "red"
 }));
 
-
-const SortByContainer = styled(Box)(({ theme }) => ({
-    height: 20,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    gap: 10,
-    margin: 20
+const SortByContainer = styled(Box)(() => ({
+  height: 20,
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "flex-end",
+  gap: 10,
+  margin: 20,
 }));
 
-
-const VerticalLine = styled(Box)(({ theme }) => ({
-    borderLeft: "1px solid black",
-    height: 25
+const VerticalLine = styled(Box)(() => ({
+  borderLeft: "1px solid black",
+  height: 25,
 }));
 
-
-
-
-const SortByText = styled(PrimarySubText)(({ theme }) => ({
-    fontSize: 12,
+const SortByText = styled(PrimarySubText)(() => ({
+  fontSize: 12,
 }));
-
-
-
-
-
-
-
 
 export const Content = () => {
-    const isSmallScreen = useMediaQuery('(max-width:900px)');
-    return (
-        <MainContentContainer>
-            {
-                !isSmallScreen && <FilterContainer>
-                    <Filter />
-                </FilterContainer>
-            }
+  const isSmallScreen = useMediaQuery("(max-width:900px)");
+  return (
+    <MainContentContainer>
+      {!isSmallScreen && (
+        <FilterContainer>
+          <Filter />
+        </FilterContainer>
+      )}
 
-            <PartnerContainer>
-                <SortByContainer>
-                    <SortByText variant="secondaryTitle" color={"textPrimary"} >Sort By</SortByText>
-                    <VerticalLine />
-                    <SortByText variant="secondaryTitle" color={"textPrimary"} >Price</SortByText>
-                    <StyledSwitch defaultChecked />
-                    <SortByText variant="secondaryTitle" color={"primary"} >Duration</SortByText>
-                </SortByContainer>
-                <RecommendedSpecialties />
-                <EnquiryForm name="Hotel whatever from ShivajiNagar"/>
-
-            </PartnerContainer>
-
-        </MainContentContainer>
-    )
-}
+      <PartnerContainer>
+        <SortByContainer>
+          <SortByText variant="secondaryTitle" color={"textPrimary"}>
+            Sort By
+          </SortByText>
+          <VerticalLine />
+          <SortByText variant="secondaryTitle" color={"textPrimary"}>
+            Price
+          </SortByText>
+          <StyledSwitch defaultChecked />
+          <SortByText variant="secondaryTitle" color={"primary"}>
+            Duration
+          </SortByText>
+        </SortByContainer>
+        <RecommendedSpecialties />
+        {/* remove this, added for testing */}
+        <div style={{ display: "flex" }}>
+          <PackageCard
+            packageData={{
+              imgUrl: "https://picsum.photos/300",
+              logoUrl: "https://picsum.photos/200",
+              awards: ["https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200"],
+              name: "Taj Jiva Spa",
+              description: "Hath yoga package",
+            }}
+            type={"Back pain"}
+            cost={{
+              basePrice: 10000,
+              price: 1000,
+              discount: undefined,
+            }}
+            details={{
+              sessions: 20,
+              days: 9,
+            }}
+          />
+          <PackageCard
+            isIndividual
+            individualData={{
+              imgUrl: "https://picsum.photos/200",
+              isVerified: true,
+              name: "Dr Ramses",
+              exp: 27,
+              description: "BDS MDS - Naturopathy Ayurveda Hath yoga package",
+            }}
+            type="Back Pain"
+            cost={{
+              basePrice: 10000,
+              price: 7000,
+              discount: 30,
+            }}
+            details={{
+              sessions: 10,
+              days: 1,
+            }}
+          />
+        </div>
+        <EnquiryForm name="Hotel whatever from ShivajiNagar" />
+      </PartnerContainer>
+    </MainContentContainer>
+  );
+};
